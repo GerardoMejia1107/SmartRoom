76c02c4816a6f98398ff917ff9d7b3014d4f6370
GerardoMejia1107 <00104923@uca.edu.sv>
2025-11-01 08:41:20 -0600

    Agregando levantamiento básico del servidor (prueba)

---
+++ backend/src/server.ts
@@ -0,0 +1,7 @@
+import app from './app';
+import config from './config/config';
+
+
+app.listen(config.port, () => {
+    console.log(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
+})
