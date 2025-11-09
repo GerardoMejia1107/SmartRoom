4fc75a12e9b68817dfcf87ab12970a3861a4d81b
GerardoMejia1107 <00104923@uca.edu.sv>
2025-11-09 17:26:37 -0600

    Agregando service, model, controller y route para sensors

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import {Router} from "express";
 
 import userRoutes from "./user_routes";
 import access_logs_routes from "./access_logs_routes";
+import sensors_routes from "./sensors_routes";
 
 const router = Router();
 
 router.use("/users", userRoutes)
 router.use("/logs", access_logs_routes)
+router.use("/sensors", sensors_routes)
 
 export default router
